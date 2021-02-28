9eda87c852d21bed2fef6edaf98ea6cbac9c4255
trochili-y <73009761+trochili-y@users.noreply.github.com>
2021-02-28 16:29:39 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
   var map = new AMap.Map('container', {
     resizeEnable: true,
     zoom: 11,
-    center: [116.397428, 39.90923]
+    center: [113.218478, 23.403277]
 });
  //初始化定位
  map.plugin('AMap.Geolocation', function () {
